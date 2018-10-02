973f57ec720c416e19d2fd37dedd48cf30ce39c4
LAPTOP\John <johneriksson94@gmail.com>
2018-10-02 15:33:57 +0200

    added acf version 5 support

--- acf-button.php
+++ acf-button.php
@@ -1,46 +1,38 @@
 <?php
 /*
-	Plugin Name: Advanced Custom Fields: Button
-	Plugin URI: https://github.com/envex/acf-button-field
-	Description: Creates a set of button fields for the Advanced Custom Fields plugin
-	Version: 1.0.0
-	Author: Matt Vickers
-	Author URI: http://hey.facetdev.com
-	License: GPLv2 or later
-	License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Plugin Name: Advanced Custom Fields: Button
+Plugin URI: https://github.com/envex/acf-button-field
+Description: Creates a set of button fields for the Advanced Custom Fields plugin
+Version: 1.0.0
+Author: Matt Cromwell
+Author URI: http://mattcromwell.com
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
 
+// 1. set text domain
+// Reference: https://codex.wordpress.org/Function_Reference/load_plugin_textdomain
+load_plugin_textdomain( 'acf-button-field', false, dirname( plugin_basename( __FILE__ ) ) . '/lang/' );
 
-class acf_field_button_plugin
+/*
+*  register_fields
+*
+*  @description: 
+*  @since: 3.6
+*  @created: 1/04/13
+*/
+
+// version 5+
+function register_fields_v5()
 {
-	/*
-	*  Construct
-	*
-	*  @description: 
-	*  @since: 3.6
-	*  @created: 1/04/13
-	*/
-	
-	function __construct()
-	{
-		// version 4+
-		add_action('acf/register_fields', array($this, 'register_fields'));	
+	include_once('button-v5.php');
+}
+add_action('acf/include_field_types', 'register_fields_v5');
 
-	}
-	
-	/*
-	*  register_fields
-	*
-	*  @description: 
-	*  @since: 3.6
-	*  @created: 1/04/13
-	*/
-	
-	function register_fields()
-	{
-		include_once('button-v4.php');
-	}
-	
+// version 4+
+function register_fields_v4()
+{
+	include_once('button-v4.php');
 }
 
-new acf_field_button_plugin();
+add_action('acf/register_fields', 'register_fields_v4');	
